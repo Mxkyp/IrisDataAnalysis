67f1d25de14d78cbb100a4d79c313de1b5ea01c0
Mxkyp <mikolaj19@poczta.fm>
2024-10-29 15:57:49 +0100

    added a func to compute sepal characteristics

--- src/zad1.py
+++ src/zad1.py
@@ -2,25 +2,31 @@
 import numpy as np
 import pandas as pd
 import infoManagment as im
+import plotInfo as pl
 import matplotlib.pyplot
+
+SETOSA = 0
+VERSICOL = 1
+VIRGI = 2
+TOTAL = 3
 
 
 def getIrisPopulationInfo(data):
     """get iris population and % values per type"""
-    setosa = data[data["type"].isin([0])].copy()
-    versicolor = data[data["type"].isin([1])].copy()
-    virginica = data[data["type"].isin([2])].copy()
+    setosa = data[data["type"].isin([SETOSA])].copy()
+    versicolor = data[data["type"].isin([VERSICOL])].copy()
+    virginica = data[data["type"].isin([VIRGI])].copy()
 
     totalSize = len(setosa) + len(versicolor) + len(virginica)
 
-    return totalSize, len(setosa), len(versicolor), len(virginica)
+    return  len(setosa), len(versicolor), len(virginica), totalSize
 
 def getPopulationStats(populationStats):
     """format to for display in table"""
-    totalSize = populationStats[0]
-    setosaNumber = populationStats[1]
-    versicolorNumber = populationStats[2]
-    virginicaNumber = populationStats[3]
+    setosaNumber = populationStats[SETOSA]
+    versicolorNumber = populationStats[VERSICOL]
+    virginicaNumber = populationStats[VIRGI]
+    totalSize = populationStats[TOTAL]
 
     ratio0 = setosaNumber/totalSize * 100;
     ratio1 = versicolorNumber/totalSize * 100;
@@ -39,6 +45,63 @@ def getPopulationStats(populationStats):
 
     return formated_table.to_html()
 
+def getCharacteristics(data):
+    getSepalLengthStats(data)
+    getSepalWidthStats(data)
+    getPetalLengthStats(data)
+    getPetalWidthStats(data)
+
+
+def getSepalLengthStats(data):
+    """get the minimum, maximum, arithmetic mean, median"""
+    lengthCol= "length of the sepal(cm)"
+    typeCol = "type"
+    lengthData = data[[lengthCol, typeCol]]
+
+
+    total = {
+        "min":   np.min(lengthData[lengthCol]),
+        "mean":  np.mean(lengthData[lengthCol]),
+        "std":   np.std(lengthData[lengthCol]),
+        "median":np.median(lengthData[lengthCol]),
+        "mQ1":   np.percentile(lengthData[lengthCol], 25),
+        "mQ3":   np.percentile(lengthData[lengthCol], 75)
+    }
+
+    s = lengthData[typeCol].isin([SETOSA]) #  only data about setosa's length
+    setosa = {
+        "min":   np.min(lengthData[s][lengthCol]),
+        "mean":  np.mean(lengthData[s][lengthCol]),
+        "std":   np.std(lengthData[s][lengthCol]),
+        "median":np.median(lengthData[s][lengthCol]),
+        "mQ1":   np.percentile(lengthData[s][lengthCol], 25),
+        "mQ3":   np.percentile(lengthData[s][lengthCol], 75)
+    }
+
+    ve = lengthData[typeCol].isin([VERSICOL]) #  only data about versicolor's length
+    versicolor = {
+        "min":   np.min(lengthData[ve][lengthCol]),
+        "mean":  np.mean(lengthData[ve][lengthCol]),
+        "std":   np.std(lengthData[ve][lengthCol]),
+        "median":np.median(lengthData[ve][lengthCol]),
+        "mQ1":   np.percentile(lengthData[ve][lengthCol], 25),
+        "mQ3":   np.percentile(lengthData[ve][lengthCol], 75)
+    }
+
+    vi = lengthData[typeCol].isin([VIRGI]) #  only data about virginica's length
+    virginica = {
+        "min":   np.min(lengthData[vi][lengthCol]),
+        "mean":  np.mean(lengthData[vi][lengthCol]),
+        "std":   np.std(lengthData[vi][lengthCol]),
+        "median":np.median(lengthData[vi][lengthCol]),
+        "mQ1":   np.percentile(lengthData[vi][lengthCol], 25),
+        "mQ3":   np.percentile(lengthData[vi][lengthCol], 75)
+    }
+
+    return  setosa, versicolor, virginica, total
+
+
+
 
 
 def main():
@@ -48,10 +111,13 @@ def main():
     RESULTS_FILE_NAMES = (["populationTable.html", "characteristics.html", ])
 
     data = im.read_csv(DATA_LOC, DATA_COL_NAMES)
+
     popStats = getIrisPopulationInfo(data)
-    popStatsFormated = formatPopulationStats(popStats)
+    popStatsFormated = getPopulationStats(popStats)
 
     im.writeToHtml(popStatsFormated, 'table.html')
+    lengthInformation = getSepalLengthStats(data)
+    print(lengthInformation[3])
     return data
 
 main()
